dd805605f77adfaecd5c587d947a87505817e616
RennanD <rplaykl@gmail.com>
2020-02-17 22:25:14 -0300

    feat: Configure Reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import './config/reactotronConfig';
+
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
