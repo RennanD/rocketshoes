6d5d68380e18adbeea96ae9c82aafcd786bcf932
RennanD <rplaykl@gmail.com>
2020-02-21 19:18:40 -0300

    fix: Android incompatibility

--- mobile/src/utils/format.js
+++ mobile/src/utils/format.js
@@ -1,4 +1,7 @@
-export const { format: formatPrice } = new Intl.NumberFormat('pt-br', {
+import Intl from 'intl';
+import ptBR from 'intl/locale-data/jsonp/pt-BR';
+
+export const { format: formartPrice } = new Intl.NumberFormat(ptBR, {
   style: 'currency',
   currency: 'BRL',
 });
